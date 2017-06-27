fba11bdffaf2bdf8e1e4d8b1cee28a7df3cc3389
ratansen <ratangsenapathy@gmail.com>
2017-06-27 14:01:46 -0700

    Basic play field created and rigged turtle prefab added to scene

---
+++ Assets/Scripts/TurtleController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleController : MonoBehaviour {
+
+	// Use this for initialization
+	public float landSpeed;
+	public float waterSpeed;
+
+	private float speed;
+	private Rigidbody rb;
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		speed = 0.02f;
+	}
+	
+	float m_distanceTraveled = 0.0f;
+
+	void Update() {
+		if (m_distanceTraveled < 100f) {
+			Vector3 oldPosition = transform.position;
+			Debug.Log (transform.right);
+			transform.Translate(-transform.right*speed * Time.deltaTime);
+			m_distanceTraveled += Vector3.Distance(oldPosition, transform.position);
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Fence") {
+			
+		}
+	}
+}
