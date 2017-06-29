2eee3ff2fc5b1fb0313230af0cdaa07942733069
ratansen <ratangsenapathy@gmail.com>
2017-06-28 22:11:14 -0700

    Add for prefab

--- Assets/Scripts/TurtleController.cs
+++ Assets/Scripts/TurtleController.cs
@@ -15,20 +15,32 @@ public class TurtleController : MonoBehaviour {
 		speed = 0.02f;
 	}
 	
-	float m_distanceTraveled = 0.0f;
+
 
 	void Update() {
-		if (m_distanceTraveled < 100f) {
+		Debug.Log (transform.rotation);
+//		if (transform.rotation.y > 45) {
+//			transform.rotation = Quaternion.Euler (new Vector3 (transform.rotation.x,0.0f,transform.rotation.z));
+//		}
 			Vector3 oldPosition = transform.position;
-			Debug.Log (transform.right);
-			transform.Translate(-transform.right*speed * Time.deltaTime);
-			m_distanceTraveled += Vector3.Distance(oldPosition, transform.position);
-		}
+			//Debug.Log (transform.right);
+			transform.Translate(-transform.right*speed * Time.deltaTime,Space.World);
+			//m_distanceTraveled += Vector3.Distance(oldPosition, transform.position);
+		
 	}
 
 	void OnTriggerEnter(Collider other){
 		if (other.tag == "Fence") {
-			
+			StartCoroutine (RotateMe (Vector3.up * 30, 10));
+		}
+	}
+
+	IEnumerator RotateMe(Vector3 byAngles, float inTime) {
+		var fromAngle = transform.rotation;
+		var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
+		for(var t = 0f; t < 1; t += Time.deltaTime/inTime) {
+			transform.rotation = Quaternion.Lerp(fromAngle, toAngle, t);
+			yield return null;
 		}
 	}
 }
---
+++ Assets/Scripts/TurtleGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleGenerator : MonoBehaviour {
+
+	public Terrain terrain;
+	// Use this for initialization
+	private Vector3 startCoordinate;
+	private Vector3 endCoordinate;
+	void Start () {
+		startCoordinate = terrain.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
